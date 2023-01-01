745ff626341f3355ad41ab531a251215c41e6486
n.mavrychev <n.mavrychev@b2b-center.ru>
2023-01-01 21:42:10 +0300

    Second Step. Arguments and option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,10 @@ const program = new Command();
 
 program.name('gendiff').description('CLI gendiff').version('0.0.1');
 
-program.description('Compares two configuration files and shows a difference.');
+program
+  .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath1>')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
